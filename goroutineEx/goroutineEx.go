de6ee8e175b8cbce7142c1952f5f0fbe8531c525
Stone.F.Yang <Stone.F.Yang@newegg.com>
2017-06-22 11:28:55 +0800

    20170622-2 p68 ch7.2 Start

--- goroutineEx/goroutineEx.go
+++ goroutineEx/goroutineEx.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"math"
+	"runtime"
 	"sync"
 )
 
 func main() {
-	test1()
+	//test1()
+	//test12()
+	//test2()
+	test3()
 }
 
-///
+///WaitGroup
 func sum(id int) {
 	var x int64
 	for i := 0; i < math.MaxUint32; i++ {
@@ -20,12 +24,58 @@ func sum(id int) {
 
 func test1() {
 	wg := new(sync.WaitGroup)
-	wg.Add(2)
-	for i := 0; i < 2; i++ {
+	wg.Add(3)
+	for i := 0; i < 3; i++ {
 		go func(id int) {
-			defer wg.Done()
+			defer wg.Done() // = Add(-1)
 			sum(id)
 		}(i)
 	}
+	wg.Wait()
+}
+
+func test12() {
+	for i := 0; i < 3; i++ {
+		sum(i)
+	}
+}
+
+///Goexit
+func test2() {
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		defer println("A.defer")
+		func() {
+			defer println("B.defer")
+			runtime.Goexit() // 终止当前 goroutine
+			println("B")     // 不会执行
+		}()
+		println("A") // 不会执行
+	}()
+	wg.Wait()
+}
+
+///
+func test3() {
+	wg := new(sync.WaitGroup)
+	wg.Add(2)
+	var A = func() {
+		defer wg.Done()
+		for i := 0; i < 6; i++ {
+			println(i)
+			if i == 3 {
+				runtime.Gosched()
+			}
+		}
+	}
+	var B = func() {
+		defer wg.Done()
+		println("Hello, World!")
+	}
+	go B()
+	go A()
+
 	wg.Wait()
 }
