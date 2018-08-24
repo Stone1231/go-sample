1f0eb0657962ed4c063e349857b0b79fc04cc320
Stone.F.Yang (netw it.twtp01.Newegg) 61589 <sy43@buyabs.corp>
2018-08-24 10:07:25 +0800

    my flattenTree

--- leecode/flattenTree_test.go
+++ leecode/flattenTree_test.go
@@ -78,14 +78,49 @@ func flatten(root *TreeNode) {
 	root.Right = tmpRight
 }
 
+func flattenMy(root *TreeNode) {
+	if (root == nil) || (root.Left == nil && root.Right == nil) {
+		return
+	}
+
+	if root.Left != nil {
+		flattenMy(root.Left)
+	}
+
+	if root.Right != nil {
+		flattenMy(root.Right)
+	}
+	
+	left := root.Left
+	if left != nil {
+		right := root.Right
+		root.Right = left
+		root.Left = nil
+		for left.Right != nil {
+			left = left.Right
+		}
+		left.Right = right
+	}
+}
+
 func Test_flatten(t *testing.T) {
 	node := NewTree(1)
 	node.LR(2, 5).R(6)
 	node.Left.LR(3, 4)
 	flatten(node)
 	node.Print()
+}
 
-	node = NewTree(6)
+func Test_flattenMy(t *testing.T) {
+	node := NewTree(1)
+	node.LR(2, 5).R(6)
+	node.Left.LR(3, 4)
+	flattenMy(node)
+	node.Print()
+}
+
+func Test_sortTree(t *testing.T) {
+	node := NewTree(6)
 	node.L(2).RL(3, 4).L(5)
 	sortTree(&node)
 	node.Print()
