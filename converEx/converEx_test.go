78ff0ac0a07eab7ce2fc44261c585f8cae7040bc
Stone.F.Yang (netw it.twtp01.Newegg) 61589 <sy43@buyabs.corp>
2018-08-29 16:02:13 +0800

    course schedule

--- converEx/converEx_test.go
+++ converEx/converEx_test.go
@@ -3,6 +3,7 @@ package converEx
 import (
 	"fmt"
 	"testing"
+	"strconv"
 )
 
 func Test_ByteToInt(t *testing.T) {
@@ -10,4 +11,10 @@ func Test_ByteToInt(t *testing.T) {
 	// var n int = b // Error: cannot use b (type byte) as type int in assignment
 	var n int = int(b) //
 	fmt.Println(n)
+}
+
+func Test_intToStr(t *testing.T) {
+	n := 11
+	var s string = "it is " + strconv.Itoa(n)
+	fmt.Println(s)
 }
--- leecode/courseSchedule_test.go
+++ leecode/courseSchedule_test.go
@@ -81,29 +81,22 @@ func canFinish(
 	numCourses int,
 	prerequisites [][]int) bool {
 
-	fmt.Print("courses:")
+	fmt.Println("courses:")
 	fmt.Println(prerequisites)
 
-	G := make([][]int, numCourses)	
-	degree := make([]int, numCourses)
+	G := make([][]int, numCourses)//directed graph	
+	degree := make([]int, numCourses)//點的邊數
 
 	for _, item := range prerequisites {
+		// i -> j
 		i := item[0]
 		j := item[1]
 		G[i] = append(G[i],j)
 		degree[j]++
 	}
 
-	fmt.Println("index所依靠的課程們")
-	fmt.Print("G:")
-	fmt.Println(G)
-	fmt.Println("")
+	fmt.Printf("\nG: %v \n", G)
 
-	fmt.Println("index被依靠的課程數量")
-	fmt.Print("degree:")
-	fmt.Println(degree)
-	fmt.Println("")
-	
 	bfs := []int{}
 
 	for index := 0; index < numCourses; index++ {
@@ -112,37 +105,42 @@ func canFinish(
 		}
 	}
 
-	fmt.Println("從來沒被依靠的index")
-	fmt.Print("bfs-1:")
-	fmt.Println(bfs)
-	fmt.Println("")
-
+	fmt.Printf("\ndegree: %v \n", degree)
+	fmt.Printf("bfs: %v 廣度搜尋無邊資料 degree[index] == 0 \n", bfs)
+	
+	fmt.Println("\nforLoop bfs:")
 	for i := 0; i < len(bfs); i++ {
+		fmt.Printf("bfs[%v]: %v \n", i, bfs[i])
+		fmt.Printf("	G[%v]: %v \n", bfs[i], G[bfs[i]])
 		for _, j := range G[bfs[i]] {
 			degree[j]--
+			fmt.Printf("		degree[%v]-1: %v \n", j, degree)
 			if (degree[j] == 0) {
+				//這邊的新增也會進入到bfs for執行
 				bfs = append(bfs, j)
+				fmt.Printf("			bfs append %v: %v  \n", j, bfs)
 			}
 		}
+		
+		fmt.Println("---------")
 	}
 
-	fmt.Println("處理後所有的課")
-	fmt.Print("bfs-2:")
-	fmt.Println(bfs)
+	fmt.Printf("\nfinally: \n")
+	fmt.Printf("degree: %v \n", degree)
+	fmt.Printf("bfs: %v \n", bfs)
+
+	res := len(bfs) == numCourses
 	fmt.Println("")
-
-	fmt.Println("處理後都沒有index被依靠的課程數量")
-	fmt.Print("degree-2:")
-	fmt.Println(degree)
-	fmt.Print("")
-
 	fmt.Println("len(bfs) == numCourses")
-	return len(bfs) == numCourses
+
+	return res
 }
 
 func Test_canFinish(t *testing.T) {
 	//fmt.Println(canFinish(4, [][]int{{1, 0}, {2, 0}, {3, 2}}))
-	fmt.Println(canFinish(5, [][]int{{1, 0}, {2, 0}, {3, 2}, {3, 0}}))
+	
+	//fmt.Println(canFinish(5, [][]int{{1, 0}, {2, 0}, {3, 2}, {3, 0}}))
+	fmt.Println(canFinish(5, [][]int{{1, 0}, {2, 0}, {3, 2}, {3, 0}, {4, 1}, {0, 4}}))
 
 	// println(canFinish(1, [][]int{}))
 	// println(canFinish(3, [][]int{{1, 0}, {0, 2}, {2, 1}}))
