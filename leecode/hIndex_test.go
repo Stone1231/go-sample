9340e87f36624a25de561c3533a3846f4f03f1d0
Stone.F.Yang (netw it.twtp01.Newegg) 61589 <sy43@buyabs.corp>
2018-08-31 14:02:12 +0800

    sort - hIndex

---
+++ leecode/hIndex_test.go
@@ -0,0 +1,26 @@
+package leecode
+
+import (
+	"fmt"
+	"sort"
+	"testing"
+)
+
+func hIndex(citations []int) int {
+
+	sort.Ints(citations)
+
+	count := len(citations)
+
+	for i := 0; i < count; i++ {
+		if citations[i] >= count-i {
+			return count - i
+		}
+	}
+
+	return 0
+}
+
+func Test_hIndex(t *testing.T) {
+	fmt.Println(hIndex([]int{3, 0, 6, 1, 5}))
+}
